40c93ac207b8a478ade6b21e05de0bd1e8205fc4
Arnab Majumder <123185510+arnab369@users.noreply.github.com>
2024-09-01 18:40:30 +0530

    Add files via upload

--- GameFrame.java
+++ GameFrame.java
@@ -1,14 +1,41 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class GameFrame extends JFrame {
 
-    GameFrame() {
+    public void level1() {
         this.add(new GamePanel());
         this.setTitle("Snake");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
         this.pack();
-        this.setVisible(true);
         this.setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("snake.jpg");
+        this.setIconImage(icon.getImage());
+        this.setVisible(true);
     }
+
+    public void level2() {
+        this.add(new Level2());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("snake.jpg");
+        this.setIconImage(icon.getImage());
+        this.setVisible(true);
+    }
+
+    public void level3() {
+        this.add(new Level3());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setLocationRelativeTo(null);
+        ImageIcon icon = new ImageIcon("snake.jpg");
+        this.setIconImage(icon.getImage());
+        this.setVisible(true);
+    }
+
 }
--- SnakeGame.java
+++ SnakeGame.java
@@ -1,7 +1,33 @@
+import java.util.Scanner;
+
 public class SnakeGame {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your choice: ");
+        int choice = sc.nextInt();
 
-        new GameFrame();
+        switch (choice) {
+            case 1:
+                GameFrame easyLevel = new GameFrame();
+                easyLevel.level1();
+                break;
+
+            case 2:
+                GameFrame mediumLevel = new GameFrame();
+                mediumLevel.level2();
+                break;
+
+            case 3:
+                GameFrame difficultLevel = new GameFrame();
+                difficultLevel.level3();
+                break;
+
+            case 4:
+                break;
+
+            default:
+                System.out.println("Invalid Level Input");
+        }
     }
 }
